9f8255c4b66fab0a3bf3ae43c97518fdfa7b8d1d
colodoo <lodog1@qq.com>
2018-09-23 20:26:55 +0800

    初始化项目

---
+++ 04_actionsheet/actionsheet.js
@@ -0,0 +1,52 @@
+let runBtn = document.getElementById('run')
+let hideBtn = document.getElementById('hide')
+
+let Dialog = function (opts) {
+
+  this.elem = opts.elem || null
+  this.comp = opts.comp || 'dialog'
+
+  this.mask = opts.mask || document.createElement('div')
+  this.body = opts.parent || document.body
+
+  this.init = () => {
+    if (this.elem != null) {
+      this.elem = document.getElementById(this.elem)
+      this.mask.addEventListener('click', (e) => {
+        this.elem.className = this.comp
+        this.mask.className = ''
+      })
+      this.body.appendChild(this.mask)
+    }
+  }
+
+  this.run = () => {
+    if (this.elem != null) {
+      this.elem.className = this.comp + ' ' + this.comp + '__run'
+      this.mask.className = 'mask'
+    }
+  }
+
+  this.hide = () => {
+    if (this.elem != null) {
+      this.elem.className = this.comp
+      this.mask.className = ''
+    }
+  }
+
+  this.init()
+
+}
+
+var dlg = new Dialog({
+  elem: 'action-sheet',
+  comp: 'action-sheet'
+})
+
+runBtn.addEventListener('click', (e) => {
+  dlg.run()
+})
+
+hideBtn.addEventListener('click', (e) => {
+  dlg.hide()
+})
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# webapp_30day
-记录学习webapp30天
+# webapp_30day  
+
+## 前言  
+  
+之前写过一个webapp的项目，奈何前端技术渣，只能用UI框架复制粘贴，这次用记录的方式学习一下前端的布局，以及整理一些简单的组件，方便以后使用。  
+
+> 不采用框架方式编写，意在理解各个组件的原理。
+
+  
+## 目录  
+
+- [01_layout](./01_layout/layout.html)  
+- [02_toolbar](./02_toolbar/toolbar.html)  
+- [03_tabbar](./03_tabbar/tabbar.html)  
+- [04_actionsheet](./04_actionsheet/actionsheet.html) 
+- [05_dialog](./05_dialog/dialog.html) 
+
+## 社区
+
+- [组织](https://github.com/lanb-code)  
+- [加入讨论](https://github.com/colodoo/webapp_30day/issues)  
+
+
+## 许可证书  
+
+[MIT](https://github.com/colodoo/webapp_30day/blob/master/LICENSE)
